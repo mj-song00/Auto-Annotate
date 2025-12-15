ed95fec7545b4651b294ba70d2e642c29221aca2
mj-song00 <niveus128@gmail.com>
2025-12-15 09:24:02 +0900

    init: 초기 설정

---
+++ src/main/java/auto/annotate/AnnotateApplication.java
@@ -0,0 +1,13 @@
+package auto.annotate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/auto/annotate/AnnotateApplicationTests.java
@@ -0,0 +1,13 @@
+package auto.annotate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
