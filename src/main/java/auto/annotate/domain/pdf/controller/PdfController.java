b6e409ebcba163d54132965980bd7deb97d73450
mj-song00 <niveus128@gmail.com>
2025-12-17 22:50:35 +0900

    feat: pdf 미리보기 기능 추가

---
+++ src/main/java/auto/annotate/domain/pdf/controller/PdfController.java
@@ -0,0 +1,34 @@
+package auto.annotate.domain.pdf.controller;
+
+import auto.annotate.domain.document.repository.DocumentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import auto.annotate.domain.document.entity.Document;
+
+@Controller
+public class PdfController {
+
+    private final DocumentRepository documentRepository;
+
+    public PdfController(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    @GetMapping("/")
+    public String pdfPreview(Model model) {
+
+        List<UUID> documentIds = documentRepository.findAll()
+                .stream()
+                .map(Document::getId)
+                .collect(Collectors.toList());
+
+        model.addAttribute("documentIds", documentIds);
+        return "pdf_preview";
+    }
+}
