9b2e8b2f196ce8a4fe84a91eedc65b5dc6b38d2b
mj-song00 <niveus128@gmail.com>
2026-01-28 10:46:54 +0900

    chore: 응답 enum 추가

--- src/main/java/auto/annotate/common/exception/ExceptionEnum.java
+++ src/main/java/auto/annotate/common/exception/ExceptionEnum.java
@@ -17,9 +17,18 @@ public enum ExceptionEnum {
     FILE_WRITE_ERROR(HttpStatus.BAD_REQUEST, "FILE_WRITE_ERROR","pdf를 수정하던중 오류가 발생하였습니다" ),
     FILE_NOT_FOUND(HttpStatus.BAD_REQUEST,"FILE_NOT_FOUND", "file을 찾지 못했습니다" ),
     FILE_SAVE_FAILED(HttpStatus.BAD_REQUEST, "FILE_SAVE_FAILED","저장이 실패하였습니다." ),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST,"USER_NOT_FOUND" , "유저를 확인할 수 없습니다." );
+
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST,"USER_NOT_FOUND" , "유저를 확인할 수 없습니다." ),
+    USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "HttpStatus.BAD_REQUEST","이미 존재하는 계정입니다."),
+    ALREADY_DELETED(HttpStatus.BAD_REQUEST, "ALREADY_DELETED", "탈퇴된 사용자입니다."),
+    EMAIL_OR_PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "EMAIL_PASSWORD_MISMATCH", "이메일 혹은 비밀번호가 일치하지 않습니다."),
+    PASSWORD_SAME_AS_OLD(HttpStatus.BAD_REQUEST, "PASSWORD_SAME_AS_OLD", "새 비밀번호가 기존 비밀번호와 동일합니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "PASSWORD_MISMATCH", "비밀번호가 일치하지 않습니다."),
+    NICKNAME_SAME_AS_OLD(HttpStatus.BAD_REQUEST, "NICKNAME_SAME_AS_OLD", "새로운 닉네임을 입력해주세요."),
 
 
+    // 리프레시 토큰 관련
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_REFRESH_TOKEN", "잘못된 리프레시 토큰입니다.");
     private final HttpStatus status;
     private final String errorCode;
     private final String message;
--- src/main/java/auto/annotate/common/response/ApiResponseEnum.java
+++ src/main/java/auto/annotate/common/response/ApiResponseEnum.java
@@ -4,7 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum ApiResponseEnum {
-    REGISTRATION_SUCCESS("파일 저장 완료");
+    REGISTRATION_SUCCESS("파일 저장 완료"),
+    SIGNUP_SUCCESS("회원가입 완료"),
+    PROFILE_RETRIEVED_SUCCESS("프로필 조회가 완료"),
+    PASSWORD_CHANGED_SUCCESS("비밀번호 변경 완료"),
+    NICKNAME_CHANGED_SUCCESS("닉네임 변경 완료"),
+    USER_DELETED_SUCCESS("회원 탈퇴가 완료");
 
     private final String message;
 
