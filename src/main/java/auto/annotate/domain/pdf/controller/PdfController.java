daf62d6f01643f6ae3a15b9ad25b89161bddf31a
mj-song00 <niveus128@gmail.com>
2026-01-30 11:18:27 +0900

    feat: login 화면 렌더링 추가

--- src/main/java/auto/annotate/domain/pdf/controller/PdfController.java
+++ src/main/java/auto/annotate/domain/pdf/controller/PdfController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PdfController {
 
     @GetMapping("/")
-    public String pdfPreview() {
+    public String root() {
+        return "redirect:/login";
+    }
 
+    @GetMapping("/login")
+    public String login() {
         return "login";
     }
 
