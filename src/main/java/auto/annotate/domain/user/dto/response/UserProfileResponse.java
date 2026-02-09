de9b6c3dbfdf789d072779f0d949e55ae7f5d735
mj-song00 <niveus128@gmail.com>
2026-02-09 16:47:21 +0900

    chore: 프로필 response email 제거

--- src/main/java/auto/annotate/domain/user/dto/response/UserProfileResponse.java
+++ src/main/java/auto/annotate/domain/user/dto/response/UserProfileResponse.java
@@ -8,19 +8,16 @@ import java.util.UUID;
 @Getter
 public class UserProfileResponse {
     private UUID id;
-    private String email;
     private String nickName;
 
-    public UserProfileResponse(UUID id, String email, String nickName) {
+    public UserProfileResponse(UUID id, String nickName) {
         this.id = id;
-        this.email = email;
         this.nickName = nickName;
     }
 
     public static UserProfileResponse of (User user) {
         return new UserProfileResponse(
                 user.getId(),
-                user.getEmail(),
                 user.getNickName()
         );
     }
