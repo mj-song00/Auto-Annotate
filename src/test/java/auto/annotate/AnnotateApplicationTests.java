e45f1cf738e065c9974b150ba7dfc30e49b8c5ca
mj-song00 <niveus128@gmail.com>
2026-02-26 18:33:56 +0900

    feat: 스케쥴링 설정

--- src/test/java/auto/annotate/AnnotateApplicationTests.java
+++ src/test/java/auto/annotate/AnnotateApplicationTests.java
@@ -2,8 +2,10 @@ package auto.annotate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
+@EnableScheduling
 class AnnotateApplicationTests {
 
     @Test
