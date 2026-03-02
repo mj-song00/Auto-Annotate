9bce4c83bc828a74e593700fde1c91646ac951ff
mj-song00 <niveus128@gmail.com>
2026-03-02 15:47:21 +0900

    refacotr: 사용되지 않는 contoller 정리

--- src/main/java/auto/annotate/domain/document/controller/DocumentController.java
+++ src/main/java/auto/annotate/domain/document/controller/DocumentController.java
@@ -10,6 +10,8 @@ import auto.annotate.domain.document.repository.DocumentRepository;
 import auto.annotate.domain.document.service.DocumentService;
 import auto.annotate.domain.folder.dto.request.SaveFolderRequest;
 import auto.annotate.domain.user.dto.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -20,11 +22,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
+@Tag(name = "Document", description = "PDF파일 업로드 및 분석결과 엑셀 다운로드 API")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -35,6 +36,7 @@ public class DocumentController {
     private final DocumentService documentService;
     private final DocumentRepository documentRepository;
 
+    @Operation(summary = "PDF파일 업로드", description = "폴더 명과 파일을 업로드하여 분석을 진행합니다.")
     @PostMapping(value="/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -56,6 +58,7 @@ public class DocumentController {
      * (현재 프론트에서 사용하지 않음)
      * 향후 하이라이트 PDF 확인 기능을 위해 유지 중.
      */
+    @Operation(summary = "하이라이트 생성(현재 사용중이지 않음)", description = "조건에 맞는 하이라이트를 생성하여 PDF뷰어에 표시합니다. 현재는 사용되고 있지 않습니다.")
     @GetMapping("/{documentId}/highlighted")
     public ResponseEntity<Resource> getHighlightedDocument(
             @PathVariable UUID documentId,
@@ -79,20 +82,7 @@ public class DocumentController {
                 .body(resource);
     }
 
-    @GetMapping
-    public List<Map<String, Object>> getAllDocumentIds() {
-        return documentRepository.findAll()
-                .stream()
-                .map(doc -> {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("id", doc.getId());
-                    map.put("filePath", doc.getFileUrl());
-                    map.put("originalFileName", doc.getOriginalFileName());
-                    return map;
-                })
-                .toList();
-    }
-
+    @Operation(summary = "분석된 파일 다운로드", description = "조건에 맞는 파일들을 다운로드 합니다.")
     @GetMapping("/{documentId}/excel")
     public ResponseEntity<Resource> downloadExcelByCondition(
             @PathVariable UUID documentId,
